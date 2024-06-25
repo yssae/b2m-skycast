912ddc44f7ad4c3ef9b79eb00dc509ab909d1bf4
Alyssa Kate Elesterio <alyssakate.elesterio@dxc.com>
2024-06-26 04:15:25 +0800

    Update models with air polution related interfaces

--- src/app/shared/models/weather.ts
+++ src/app/shared/models/weather.ts
@@ -54,3 +54,33 @@ interface Sys {
     sunrise: number;
     sunset: number;
 }
+
+export interface AirQualityDescription {
+    score: number,
+    name: string,
+    description: string
+}
+
+export interface AirPollutionData {
+    coord: {
+        lon: number;
+        lat: number;
+    };
+    list: {
+        main: {
+            aqi: number;
+        };
+        components: {
+            co: number;
+            no: number;
+            no2: number;
+            o3: number;
+            so2: number;
+            pm2_5: number;
+            pm10: number;
+            nh3: number;
+        };
+        dt: number;
+    }[],
+    aqiDescription?: AirQualityDescription
+}
