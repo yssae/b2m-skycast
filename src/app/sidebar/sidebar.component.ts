abc2188d18a6a5b0507a17d8324d05de7e1e574e
Alyssa Kate Elesterio <alyssakate.elesterio@dxc.com>
2024-06-26 04:17:09 +0800

    Update sidebar component to display data

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../shared/services/weather.service';
+import { WeatherData } from '../shared/models/weather';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'sc-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
-  options: any;
+export class SidebarComponent implements OnInit {
+  private ngUnsubscribe = new Subject<void>();
+  public currentWeatherData!: WeatherData;
+  
+  constructor(public weatherService: WeatherService) {}
+
+  public ngOnInit(): void {
+    this.weatherService.currentWeather$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((weather: WeatherData) => {
+      this.currentWeatherData = weather;
+    });
+    this.weatherService.currentAirQualityData$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((air: any) => {
+      console.log('air', air)
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.unsubscribe();
+  }
+
+  public toggleToCelsius() {
+    this.weatherService.isInCelsius$.next(true);
+  }
+
+  public toggleToFahrenheit() {
+    this.weatherService.isInCelsius$.next(false);
+  }
+
+
 }
