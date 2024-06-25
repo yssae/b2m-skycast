2982367936fb758960477623a482d8d096605dae
Alyssa Kate Elesterio <alyssakate.elesterio@dxc.com>
2024-06-25 19:23:28 +0800

    Implement geolocation service and setting of background in home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil, tap } from 'rxjs';
+
+import { GeolocationService } from './shared/services/geolocation.service';
+
+import { WeatherData } from './shared/models/weather';
+import { weather } from './shared/constants';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'skycast';
+  private ngUnsubscribe = new Subject<void>();
+  public background: string = 'assets/clear-blue-sky.jpg';
+
+  constructor(private geolocationService: GeolocationService) {
+  }
+ 
+  ngOnInit(): void {
+    this.geolocationService.dispatch()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((response: WeatherData) => {
+        this.setBackground(response)
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.unsubscribe();
+  }
+
+  private setBackground(weatherData: WeatherData): void {
+    this.background = `assets/${weather.find(item => item.weatherCodes.includes(weatherData.weather[0].main.toLowerCase()))?.background}` ?? '';
+  }
 }
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,18 @@
+export const weather = [
+    {
+      background: 'clear-blue-sky.jpg',
+      weatherCodes: ["clouds", "clear"]
+    },
+    {
+      background: 'dark-cloudy-sky-jpg',
+      weatherCodes: ["atmosphere", "drizzle"]
+    },
+    {
+      background: 'snow.jpg',
+      weatherCodes: ["snow"]
+    },
+    {
+      background: 'thunderstorm.jpg',
+      weatherCodes: ["rain, thunderstorm"]
+    }
+  ]
