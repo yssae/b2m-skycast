7f5b04768b4bd4ae608b3d912027df6509de94fb
Alyssa Kate Elesterio <alyssakate.elesterio@dxc.com>
2024-06-26 04:16:15 +0800

    Add air pollution implementation in weather service

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-import { WeatherData } from '../models/weather';
+import { AirPollutionData, WeatherData } from '../models/weather';
 import { BehaviorSubject, Observable, combineLatest, distinctUntilChanged, map, switchMap, tap } from 'rxjs';
 import { GeolocationService } from './geolocation.service';
+import { AQI_DESCRIPTION } from '../constants';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,11 @@ import { GeolocationService } from './geolocation.service';
 export class WeatherService {
   private baseApiUrl = 'https://api.openweathermap.org/data/2.5';
   private weatherApiUrl = '/weather'
+  private airQualityApiUrl = '/air_pollution'
 
   public currentWeather$ = new BehaviorSubject<WeatherData>({} as WeatherData);
+  public currentAirQualityData$ = new BehaviorSubject<AirPollutionData>({} as AirPollutionData);
+
   public isInCelsius$ = new BehaviorSubject<boolean>(true);
 
   constructor(
@@ -26,7 +30,10 @@ export class WeatherService {
       this.geolocationService.getLocation().pipe(distinctUntilChanged()),
       this.isInCelsius$.pipe(distinctUntilChanged())
     ]).pipe(
-      tap(([coords, isCelsius]) => this.geolocationService.saveCoordinates(coords)),
+      tap(([coords, isCelsius]) => {
+        this.geolocationService.saveCoordinates(coords);
+        this.determineAQI(coords);
+      }),
       switchMap(([location, isCelsius]) => {
         const units = isCelsius ? 'metric' : 'standard';
         return this.getCurrentWeatherByCoords(location, units);
@@ -45,4 +52,24 @@ export class WeatherService {
     return this.http.get<WeatherData>(this.baseApiUrl+this.weatherApiUrl+query);
   }
 
+  public getAirPollutionData(position: GeolocationPosition): Observable<AirPollutionData> {
+    const query = `?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${environment.APIKEY}`;
+    return this.http.get<AirPollutionData>(this.baseApiUrl+this.airQualityApiUrl+query);
+  }
+
+  private determineAQI(position: GeolocationPosition) {
+    this.getAirPollutionData(position).pipe(
+      tap((air: AirPollutionData) => this.setAQIDescription(air)),
+      tap((air: AirPollutionData) => this.currentAirQualityData$.next(air))
+    ).subscribe();
+  }
+  
+  private setAQIDescription(air: AirPollutionData) {
+    const currentAQI = air.list[0]?.main.aqi;
+    const matchedDescription = AQI_DESCRIPTION.find(aqi => currentAQI < aqi.score);
+  
+    if (matchedDescription) {
+      air.aqiDescription = matchedDescription;
+    }
+  }
 }
