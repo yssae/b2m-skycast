37b5f19b1e6e69edc2f216bb2fd3a1d19a6d500b
Alyssa Kate Elesterio <alyssakate.elesterio@dxc.com>
2024-06-26 15:01:09 +0800

    Refactor geolocation service

--- src/app/shared/services/geolocation.service.ts
+++ src/app/shared/services/geolocation.service.ts
@@ -1,35 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Coord } from '../models/weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeolocationService {
-  
-  constructor() { }
+  public coords$ = new BehaviorSubject<Coord | null>(null);
 
-  public getLocation(): Observable<GeolocationPosition> {
-    const savedCoordinates = this.getSavedCoordinates();
-    return savedCoordinates 
-    ? of(savedCoordinates) 
-    : new Observable((observer) => {
-        navigator.geolocation.getCurrentPosition(
-            (position: GeolocationPosition) => {
-              observer.next(position);
-              observer.complete();
-            },
-            (error: GeolocationPositionError) => {
-              observer.error(error);
-            }
-        );
-      });
+  constructor() { 
+    this.getLocation();
   }
 
-  public saveCoordinates(coords: GeolocationPosition): void {
+  public getCoords$(): Observable<Coord | null> {
+    return this.coords$.asObservable();
+  }
+
+  public getLocation(): void {
+    const savedCoordinates = this.getSavedCoordinates();
+    if (savedCoordinates) {
+      this.coords$.next(savedCoordinates);
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const coords = { lat: position.coords.latitude, lon: position.coords.longitude };
+          console.log('coords geo', coords)
+          this.coords$.next(coords);
+        },
+        (error: GeolocationPositionError) => {
+          console.error(error);
+        }
+      );
+    }
+  }
+
+  public saveCoordinates(coords: Coord): void {
     localStorage.setItem('coords', JSON.stringify(coords));
   }
 
-  public getSavedCoordinates(): GeolocationPosition | null {
+  public clearCoordinates(): void {
+    localStorage.removeItem('coords');
+  }
+
+  public getSavedCoordinates(): Coord | null {
     const savedCoords = localStorage.getItem('coords');
     return savedCoords ? JSON.parse(savedCoords) : null;
   }
